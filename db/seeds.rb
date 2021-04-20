403f0a2357ccc65cdcc0f24ec5182419e5307aa6
raaynaldo <raaynaldo@hotmail.com>
2021-04-19 22:42:38 -0400

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AnswerPrompt.destroy_all
+Goal.destroy_all
+QuestionPrompt.destroy_all
+User.destroy_all
+
+ray = User.create!(first_name: "Raynaldo", last_name: "Sutisna", username: "RaynaldoSutisna", photo: "https://ca.slack-edge.com/T01LDUYQA4S-U01L764HZ4N-a689fcca7784-512")
+mauro = User.create!(first_name: "Mauro", last_name: "Dorigo", username: "MauroDorigo", photo: "https://ca.slack-edge.com/T01LDUYQA4S-U01U65LQVJN-3e9932e12acc-512")
+puts "User Prompt Done!"
+
+q1 = QuestionPrompt.create!(question: "How are you?", topic: 1)
+q2 = QuestionPrompt.create!(question: "What's your name?", topic: 2)
+q3 = QuestionPrompt.create!(question: "1 + 2 = ?", topic: 3)
+puts "Question Prompt Done!"
+
+puts q1.id
+AnswerPrompt.create!(question_prompt_id: q1.id, user_id: ray.id, answer: "good")
+AnswerPrompt.create!(question_prompt_id: q2.id, user_id: ray.id, answer: "Ray")
+AnswerPrompt.create!(question_prompt_id: q3.id, user_id: ray.id, answer: "3")
+
+AnswerPrompt.create!(question_prompt_id: q1.id, user_id: mauro.id, answer: "Very Good")
+AnswerPrompt.create!(question_prompt_id: q2.id, user_id: mauro.id, answer: "Mauro")
+AnswerPrompt.create!(question_prompt_id: q3.id, user_id: mauro.id, answer: "3")
+puts "Answer Prompt Done!"
+
+Goal.create!(user_id: ray.id, task: "task 1", is_done: true)
+Goal.create!(user_id: ray.id, task: "task 2", is_done: true)
+Goal.create!(user_id: ray.id, task: "task 3", is_done: false)
+Goal.create!(user_id: ray.id, task: "task 4", is_done: true)
+
+Goal.create!(user_id: mauro.id, task: "task 1", is_done: true)
+Goal.create!(user_id: mauro.id, task: "task 2", is_done: true)
+Goal.create!(user_id: mauro.id, task: "task 3", is_done: false)
+Goal.create!(user_id: mauro.id, task: "task 4", is_done: true)
+puts "Goal Done!"
