d696c2a0f71d60203221a8ba1dcf19f49ee39ab2
raaynaldo <raaynaldo@hotmail.com>
2021-04-19 21:18:37 -0400

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'example.com'
-    origins "*"
+    # https://the-nurturing-garden.vercel.app/
+    origins "the-nurturing-garden.vercel.app"
 
     resource "*",
       headers: :any,
