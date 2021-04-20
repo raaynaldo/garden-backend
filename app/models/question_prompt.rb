c6eaf71b1f626990678a6ec06986d53d19af215e
raaynaldo <raaynaldo@hotmail.com>
2021-04-19 23:23:06 -0400

    add enum for question_prompt

--- app/models/question_prompt.rb
+++ app/models/question_prompt.rb
@@ -1,4 +1,8 @@
 class QuestionPrompt < ApplicationRecord
   has_many :answer_prompts
   has_many :users, through: :answer_prompts
+
+  enum topic: { understanding: 1, responsibility: 2, space_to_grow: 3 }
+  #   Check this blog to understand this enum
+  #   https://dev.to/junko911/a-comprehensive-guide-to-rails-enum-5a07
 end
