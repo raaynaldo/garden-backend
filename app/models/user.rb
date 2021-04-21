391ccf0ecdc2847fc2a325362c52841dd352bde4
raaynaldo <raaynaldo@hotmail.com>
2021-04-21 11:50:32 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :answer_prompts
   has_many :question_prompts, through: :answer_prompts
   has_many :goals
+
+  validates :name, presence: true
+  validates :uid, presence: true, uniqueness: true
 end
