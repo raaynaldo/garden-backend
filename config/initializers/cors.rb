c5c24b7de68bdf704d788fcf8db7e8074a562d49
raaynaldo <raaynaldo@hotmail.com>
2021-04-21 11:59:13 -0400

    create cors link.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'example.com'
     # https://the-nurturing-garden.vercel.app/
-    origins "the-nurturing-garden.vercel.app"
+    origins Rails.application.config.allowed_cors_origins
 
     resource "*",
       headers: :any,
