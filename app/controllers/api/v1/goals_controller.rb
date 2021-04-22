040f657cf89ae52c91736f5f8f7d10be0f3f9ced
purisquiri <mau.dorigo@outlook.es>
2021-04-22 17:45:00 -0400

    skip before action in goals controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GoalsController < ApplicationController
+  skip_before_action :authorized, only: [:index, :show, :create]
+
   def index
     render json: Goal.all, root: "goals", adapter: :json, each_serializer: GoalSerializer, status: :ok
   end
