6e5ec18999e19bbd23cab3e8c899a63bcab33427
chirptotheeast <alyssa.easterly@yahoo.com>
2021-04-22 10:49:59 -0400

    seeded db with 5 mock questions p topic

--- app/models/question_prompt.rb
+++ app/models/question_prompt.rb
@@ -2,7 +2,7 @@ class QuestionPrompt < ApplicationRecord
   has_many :answer_prompts
   has_many :users, through: :answer_prompts
 
-  enum topic: { understanding: 1, responsibility: 2, space_to_grow: 3 }
+  enum topic: { gratitude: 1, inner_peace: 2, relationships: 3 }
   #   Check this blog to understand this enum
   #   https://dev.to/junko911/a-comprehensive-guide-to-rails-enum-5a07
 end
