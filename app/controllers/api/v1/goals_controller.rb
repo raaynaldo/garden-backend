9e574c1d4be2c610c01d7e93d90b812d6e8e7ecc
njerimk <nmurugikamau@gmail.com>
2021-04-23 15:04:24 -0400

    added update and delete method in goals controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -10,12 +10,25 @@ class Api::V1::GoalsController < ApplicationController
   end
 
   def create
+    puts "hi"
+    puts current_user
     goal = Goal.new(task: params[:task], user_id: current_user.id)
-
     if goal.save!
       render json: { goal: { id: goal.id } }, status: :ok
     else
       render json: { message: "save failed" }, status: :not_acceptable
     end
+  end  
+  
+  def update
+    goal = Goal.find(params[:id])
+    goal.update(task: params[:task] ? params[:task] : goal.task, is_done: params[:is_done] != nil ? params[:is_done] : goal.is_done)
+    render json: { goal: goal }, status: :ok
+  end
+
+  def destroy
+    goal = Goal.find(params[:id])
+    goal.destroy
+    render json: {message: "goal deleted"}
   end
 end
